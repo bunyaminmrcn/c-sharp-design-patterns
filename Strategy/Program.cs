1800b6c0465ee04e23b737c4a8b657a2c90bdac8
bunyaminmrcn <bunyaminmrcn@gmail.com>
2024-12-12 18:56:11 +0300

    add strategy dp

---
+++ Strategy/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Strategy;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       CustomerManager customerManager = new CustomerManager();
+       customerManager.CreditCalculatorBase = new AfterPandemicCreditCalculator();
+       customerManager.SaveCredit();
+    }
+}
+
+
+abstract class CreditCalculatorBase {
+    public abstract void Calculate();
+}
+
+class BeforePandemicCreditCalculator : CreditCalculatorBase
+{
+    public override void Calculate()
+    {
+         Console.WriteLine("Credit calculated before pandemic!");
+    }
+}
+
+class AfterPandemicCreditCalculator : CreditCalculatorBase
+{
+    public override void Calculate()
+    {
+         Console.WriteLine("Credit calculated after pandemic!");
+    }
+}
+
+class CustomerManager {
+    public CreditCalculatorBase CreditCalculatorBase { get; set;}
+    public void SaveCredit () {
+        Console.WriteLine("CustomerManager business code");
+        CreditCalculatorBase.Calculate();
+    }
+
+}
