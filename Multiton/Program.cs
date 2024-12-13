2442b0d3f968936c5b04eed5f518072d7934ea76
bunyaminmrcn <bunyaminmrcn@gmail.com>
2024-12-13 17:39:21 +0300

    add multiton dp

---
+++ Multiton/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Multiton;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Camera camera1 = Camera.GetCamera("NIKON");
+        Camera camera2 = Camera.GetCamera("NIKON");
+        Camera camera3 = Camera.GetCamera("CANON");
+        Camera camera4 = Camera.GetCamera("CANON");
+
+        Console.WriteLine(camera1.id);
+        Console.WriteLine(camera2.id);
+        Console.WriteLine(camera3.id);
+        Console.WriteLine(camera4.id);
+    }
+}
+
+
+class Camera {
+    public Guid id { get;set;}
+    static Dictionary<string, Camera> _cameras = new Dictionary<string, Camera>();
+    static object _lock = new object();
+    
+    private Camera() {
+        id = Guid.NewGuid();
+    }
+    public static Camera GetCamera(string brand) {
+        lock (_lock) {
+            if(!_cameras.ContainsKey(brand)) {
+                _cameras.Add(brand, new Camera());
+            }
+        }
+        return _cameras[brand];
+    }
+}
